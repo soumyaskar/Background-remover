59b4f31fb3a10df16436a144da4206d0632813b7
soumyaskar <166623193+soumyaskar@users.noreply.github.com>
2024-11-26 21:20:42 +0530

    Add files via upload

---
+++ remove_backg.py
@@ -0,0 +1,11 @@
+from rembg import remove
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+input_path = 'Naina.jpg'
+output_path = 'output.png'
+
+input_image = Image.open(input_path)
+
+output_image = remove(input_image)
+output_image.save(output_path)
